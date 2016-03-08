c9ae47ec79f0637d038dc8ad4fda85f3d6040aa0
Hoang.Phan <phantichhoang@gmail.com>
2016-03-08 14:13:53 +0700

    Tower App

---
+++ DayNhauHoc/src/com/daynhauhoc/example/App.java
@@ -0,0 +1,13 @@
+package com.daynhauhoc.example;
+
+/**
+ * App Bootstrap
+ * @author hoangpt
+ */
+public class App {
+  
+  
+  public static void main(String[] args) {
+    new Tower().buildTower();
+  }
+}
---
+++ DayNhauHoc/src/com/daynhauhoc/example/Tower.java
@@ -0,0 +1,56 @@
+
+package com.daynhauhoc.example;
+
+/**
+ * @author hoangpt
+ */
+public class Tower {
+
+  public int nrFloor = 8;
+  
+  /**
+   * build Tower
+   * @author hoangpt
+   */
+  void buildTower() {
+    for (int i = 1; i <= nrFloor; i++) {
+      _buildFloor(i);
+    }
+  }
+
+  /**
+   * build Each Floor
+   * @author hoangpt
+   * 
+   * @param floor 
+   */
+  private void _buildFloor(int floor) {
+    //build space
+    for (int j = 1; j < nrFloor-floor; j++) {
+      System.out.print(" ");
+    }
+    
+    /*
+    build nr
+    */
+    int middleNr = 2*floor - 1;
+    
+    //--1: build left
+    for (int k = floor; k < middleNr; k++) {
+      int h = k%10;
+      System.out.print(h);
+    }
+    
+    //--2: middle
+    System.out.print(middleNr);
+    
+    //--3: build right
+    for (int k = middleNr-1; k >= floor; k--) {
+      int h = k%10;
+      System.out.print(h);
+    }
+    
+    //end floor
+    System.out.println("");
+  }
+}
