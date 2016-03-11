eb23fdb10023896e64572f61dbf1db554423362a
Hoang.Phan <phantichhoang@gmail.com>
2016-03-11 10:38:47 +0700

    Swing Thread

---
+++ SwingThreads/src/com/daynhauhoc/swingthreads/ThreadCheck.java
@@ -0,0 +1,23 @@
+
+package com.daynhauhoc.swingthreads;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Original Thread
+ * @author hoangpt
+ */
+public class ThreadCheck implements Runnable {
+
+  @Override
+  public void run() {
+    for (int i = 0; i < 10; i++) {
+      System.out.println(i);
+      try {
+        Thread.sleep(2000);
+      } catch (InterruptedException ex) {
+      }
+    }
+  }
+}
